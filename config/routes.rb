0b9b8f931d0f2ac031c2e72068282df8cd20d03f
Paul Walsh <twentytwentyk@live.com>
2017-08-28 11:36:47 -0400

    Reoccuring Event Crud still wiring

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :events
   get 'hello_world', to: 'hello_world#index'
   get 'people', to: 'people#people'
+  delete 'event/:event_id' => 'events#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
